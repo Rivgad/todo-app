d36ddc6c324a1cd08d39b64114fe181ef27ba3f3
Rivgad <losvermo@gmail.com>
2023-09-25 16:45:34 +0300

    change main.tsx

--- src/web-client/src/main.tsx
+++ src/web-client/src/main.tsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-
-const router = createBrowserRouter([
-    
-]);
+import './index.css'
+import App from './app';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <App/>
     </React.StrictMode>,
 )
