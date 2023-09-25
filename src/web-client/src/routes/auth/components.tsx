97042426b91bd5d5939467aa9c1f317657a1ccd6
Rivgad <losvermo@gmail.com>
2023-09-25 15:59:07 +0300

    add loading animation and change hover

--- src/web-client/src/routes/auth/components.tsx
+++ src/web-client/src/routes/auth/components.tsx
@@ -1,4 +1,30 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+
+const loadingStyles = css`
+    filter: brightness(0.8);
+    cursor: progress;
+    position: relative;
+
+    &:after {
+        content: "";
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        width: 20px;
+        height: 20px;
+        margin-top: -10px;
+        margin-left: -10px;
+        border: 2px solid #fff;
+        border-top: 2px solid transparent;
+        border-radius: 50%;
+        animation: spin 1s linear infinite;
+    }
+
+    @keyframes spin {
+        0% { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
+    }
+`;
 
 export const Button = styled.button`
     width: 100%;
@@ -9,9 +35,14 @@ export const Button = styled.button`
     border-radius: 5px;
     font-size: large;
     cursor: pointer;
+    transition: 0.3s;
 
     &:hover {
-        background-color: #0056b3;
+        filter: brightness(0.8);
+    }
+
+    &:disabled {
+        ${loadingStyles}
     }
 `;
 
