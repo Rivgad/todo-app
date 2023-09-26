cf55e43ec948d8ea1b5354196b60ba1d85b93d4e
Rivgad <losvermo@gmail.com>
2023-09-26 22:04:13 +0300

    add ErrorBoundry

--- src/web-client/src/components/index.ts
+++ src/web-client/src/components/index.ts
@@ -1,4 +1,5 @@
 export { ErrorComponent } from './errorComponent';
+export { ErrorBoundry } from './errorBoundry';
 export { loading } from './styled/loading';
 export { Button, RedButton } from './styled/button';
 export { BaseLink } from './styled/baseLink'
