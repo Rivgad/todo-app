656178ecca2ddd3aa65efd34d994f317b2c0e094
Rivgad <losvermo@gmail.com>
2023-09-27 12:03:06 +0300

    add fetcher to TaskForm

--- src/web-client/src/routes/list/task/taskForm.tsx
+++ src/web-client/src/routes/list/task/taskForm.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import { Button, Input } from '../../../components';
 import { styled } from 'styled-components';
+import { useFetcher } from 'react-router-dom';
+import { Button, Input } from '../../../components';
 
 
 export const InputContainer = styled.div`
@@ -12,19 +13,26 @@ export const InputContainer = styled.div`
 
 export const TaskForm: React.FC = () => {
     const [text, setText] = useState('');
+    const fetcher = useFetcher();
 
     return (
-        <InputContainer>
-            <Input
-                style={{ flex: 1 }}
-                type="text"
-                name='name'
-                value={text}
-                onChange={(e) => setText(e.target.value)}
-                placeholder="Введите текст..." />
-            <Button type='submit' style={{ marginLeft: "1rem" }}>
-                Добавить пункт
-            </Button>
-        </InputContainer>
+        <fetcher.Form action={`.`} method='post'>
+            <InputContainer>
+                <Input
+                    style={{ flex: 1 }}
+                    type="text"
+                    name='name'
+                    value={text}
+                    onChange={(e) => setText(e.target.value)}
+                    placeholder="Введите текст..." />
+                <Button
+                    disabled={fetcher.state == "submitting"}
+                    type='submit'
+                    style={{ marginLeft: "1rem" }}
+                >
+                    Добавить пункт
+                </Button>
+            </InputContainer>
+        </fetcher.Form>
     );
 };
