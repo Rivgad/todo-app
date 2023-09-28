50317b1644d5db34f5d119c95c7970997d9e5a3e
Rivgad <losvermo@gmail.com>
2023-09-28 16:51:16 +0300

    add Task.status

--- src/web-client/src/model/task.ts
+++ src/web-client/src/model/task.ts
@@ -2,4 +2,10 @@ export interface Task {
     id: number;
     name: string;
     done: boolean;
+    status?: keyof typeof TaskStatus;
+}
+
+export enum TaskStatus {
+    Unfinished = 0,
+    Finished = 1,
 }
