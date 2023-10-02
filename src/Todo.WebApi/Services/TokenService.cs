01a9acf380d939889e609b8c4e54f52799c9c1cd
Rivgad <losvermo@gmail.com>
2023-10-02 23:51:35 +0300

    add token service

---
+++ src/Todo.WebApi/Services/TokenService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Todo.WebApi.Services;
+
+public interface ITokenService
+{
+	string CreateToken(params Claim[] claims);
+}
+
+public class JwtOptions
+{
+	public required string Issuer { get; set; }
+	public required string Audience { get; set; }
+	public required string Key { get; set; }
+
+	public SymmetricSecurityKey GetSymmetricSecurityKey() => new(Encoding.UTF8.GetBytes(Key));
+}
+
+public class TokenService : ITokenService
+{
+	private readonly JwtOptions _options;
+
+	public TokenService(IOptions<JwtOptions> options)
+	{
+		_options = options.Value;
+	}
+
+	public string CreateToken(params Claim[] claims)
+	{
+		var jwt = new JwtSecurityToken(
+			issuer: _options.Issuer,
+			audience: _options.Audience,
+			claims: claims,
+			expires: DateTime.UtcNow.Add(TimeSpan.FromMinutes(10)),
+			signingCredentials: new SigningCredentials(
+				_options.GetSymmetricSecurityKey(),
+				SecurityAlgorithms.HmacSha256));
+
+		return new JwtSecurityTokenHandler().WriteToken(jwt);
+	}
+}
