d66dd9578ddeb36fc4f3a779ab124e93ab2cef70
Rivgad <losvermo@gmail.com>
2023-10-02 12:54:29 +0300

    rename folders to Todo.*

---
+++ src/Todo.Core/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Todo.Core.Models;
+
+namespace Todo.Core;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+	public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+	{
+	}
+}
---
+++ src/Todo.Core/Models/ApplicationUser.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Todo.Core.Models;
+
+public class ApplicationUser : IdentityUser
+{
+}
