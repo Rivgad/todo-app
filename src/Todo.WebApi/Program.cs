15a0355fbbc45bc9b5726c43e29383baaa6ba87e
Rivgad <losvermo@gmail.com>
2023-10-02 23:52:49 +0300

    configure jwt auth

--- src/Todo.WebApi/Program.cs
+++ src/Todo.WebApi/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
@@ -6,6 +7,8 @@ using Npgsql;
 using System.Text;
 using Todo.Core.Context;
 using Todo.Core.Models;
+using Todo.WebApi.Endpoints;
+using Todo.WebApi.Services;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,28 +17,7 @@ var config = builder.Configuration;
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services
-	.AddAuthentication(options =>
-	{
-		options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-		options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-		options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-	})
-	.AddJwtBearer(options =>
-	{
-		options.TokenValidationParameters = new TokenValidationParameters()
-		{
-			ValidIssuer = config["JwtSettings:Issuer"],
-			ValidAudience = config["JwtSettings:Audience"],
-			IssuerSigningKey = new SymmetricSecurityKey(
-				Encoding.UTF8.GetBytes(config["JwtSettings:Key"]!)),
-			ValidateIssuer = true,
-			ValidateAudience = true,
-			ValidateLifetime = true,
-			ValidateIssuerSigningKey = true,
-			ClockSkew = TimeSpan.FromSeconds(5),
-		};
-	});
+builder.Services.Configure<JwtOptions>(config.GetSection("JwtSettings"));
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
@@ -44,11 +26,39 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 	options.UseNpgsql(connectionStringBuilder.ConnectionString, x => x.MigrationsAssembly("Todo.Migrations"));
 });
 
-builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+builder.Services.AddIdentityCore<ApplicationUser>()
 	.AddEntityFrameworkStores<ApplicationDbContext>()
 	.AddDefaultTokenProviders();
 
 builder.Services.AddAuthorization();
+builder.Services
+	.AddAuthentication(options =>
+	{
+		options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+		options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+		options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+		options.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme;
+	})
+	.AddJwtBearer(options =>
+	{
+		options.TokenValidationParameters = new TokenValidationParameters()
+		{
+			ValidateIssuer = true,
+			ValidIssuer = config["JwtSettings:Issuer"],
+
+			ValidateAudience = true,
+			ValidAudience = config["JwtSettings:Audience"],
+
+			ValidateIssuerSigningKey = true,
+			IssuerSigningKey = new SymmetricSecurityKey(
+				Encoding.UTF8.GetBytes(config["JwtSettings:Key"]!)),
+
+			ValidateLifetime = true,
+			ClockSkew = TimeSpan.FromSeconds(5),
+		};
+	});
+
+builder.Services.AddAuthServices();
 
 var app = builder.Build();
 
@@ -58,9 +68,18 @@ if (app.Environment.IsDevelopment())
 	app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
 
+app.UseHttpsRedirection();
 app.UseAuthorization();
-app.UseAuthentication();
+
+app.MapAuthEndpoints();
+
+app.MapGet(
+	"/secure",
+	[Authorize] (HttpContext context) =>
+	{
+
+		return "Super secret info!";
+	});
 
 app.Run();
