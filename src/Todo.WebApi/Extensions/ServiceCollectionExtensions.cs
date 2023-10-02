8183bc4724c96f3f996786c69c7ab2d71ee8a231
Rivgad <losvermo@gmail.com>
2023-10-03 00:19:36 +0300

    move configuration to extension

---
+++ src/Todo.WebApi/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Npgsql;
+using System.Text;
+using Todo.Core.Context;
+using Todo.Core.Models;
+using Todo.WebApi.Services;
+
+namespace Todo.WebApi.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+	public static IServiceCollection ConfigureIdentity(
+		this IServiceCollection services,
+		IConfiguration configuration)
+	{
+		services.AddDbContext<ApplicationDbContext>(options =>
+		{
+			var connectionStringBuilder = new NpgsqlConnectionStringBuilder(
+				configuration["Npgsql:ConnectionString"]);
+
+			options.UseNpgsql(
+				connectionStringBuilder.ConnectionString,
+				builder => builder.MigrationsAssembly("Todo.Migrations"));
+		});
+
+		services.AddIdentityCore<ApplicationUser>()
+			.AddEntityFrameworkStores<ApplicationDbContext>()
+			.AddDefaultTokenProviders();
+
+		return services;
+	}
+
+	public static AuthenticationBuilder ConfigureJwtAuthentication(
+		this IServiceCollection services,
+		IConfiguration configuration)
+	{
+		services.Configure<JwtOptions>(configuration.GetSection("JwtSettings"));
+
+		return services
+			.AddAuthentication(options =>
+			{
+				options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+				options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+				options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+				options.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme;
+			})
+			.AddJwtBearer(options =>
+			{
+				options.TokenValidationParameters = new TokenValidationParameters()
+				{
+					ValidateIssuer = true,
+					ValidIssuer = configuration["JwtSettings:Issuer"],
+
+					ValidateAudience = true,
+					ValidAudience = configuration["JwtSettings:Audience"],
+
+					ValidateIssuerSigningKey = true,
+					IssuerSigningKey = new SymmetricSecurityKey(
+						Encoding.UTF8.GetBytes(configuration["JwtSettings:Key"]!)),
+
+					ValidateLifetime = true,
+					ClockSkew = TimeSpan.FromSeconds(5),
+				};
+			});
+	}
+}
--- src/Todo.WebApi/Program.cs
+++ src/Todo.WebApi/Program.cs
@@ -1,14 +1,5 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using Npgsql;
-using System.Text;
-using Todo.Core.Context;
-using Todo.Core.Models;
 using Todo.WebApi.Endpoints;
-using Todo.WebApi.Services;
+using Todo.WebApi.Extensions;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,46 +8,10 @@ var config = builder.Configuration;
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.Configure<JwtOptions>(config.GetSection("JwtSettings"));
-
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-{
-	var connectionStringBuilder = new NpgsqlConnectionStringBuilder(config["Npgsql:ConnectionString"]);
-
-	options.UseNpgsql(connectionStringBuilder.ConnectionString, x => x.MigrationsAssembly("Todo.Migrations"));
-});
-
-builder.Services.AddIdentityCore<ApplicationUser>()
-	.AddEntityFrameworkStores<ApplicationDbContext>()
-	.AddDefaultTokenProviders();
+builder.Services.ConfigureIdentity(config);
 
 builder.Services.AddAuthorization();
-builder.Services
-	.AddAuthentication(options =>
-	{
-		options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-		options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-		options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-		options.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme;
-	})
-	.AddJwtBearer(options =>
-	{
-		options.TokenValidationParameters = new TokenValidationParameters()
-		{
-			ValidateIssuer = true,
-			ValidIssuer = config["JwtSettings:Issuer"],
-
-			ValidateAudience = true,
-			ValidAudience = config["JwtSettings:Audience"],
-
-			ValidateIssuerSigningKey = true,
-			IssuerSigningKey = new SymmetricSecurityKey(
-				Encoding.UTF8.GetBytes(config["JwtSettings:Key"]!)),
-
-			ValidateLifetime = true,
-			ClockSkew = TimeSpan.FromSeconds(5),
-		};
-	});
+builder.Services.ConfigureJwtAuthentication(config);
 
 builder.Services.AddAuthServices();
 
