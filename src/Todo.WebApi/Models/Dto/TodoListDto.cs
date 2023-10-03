dcdf06eae1585518ba9d7366abbd04636daef6fc
Rivgad <losvermo@gmail.com>
2023-10-03 23:16:19 +0300

    add dtos and dto convert extenstions

---
+++ src/Todo.WebApi/Models/Dto/TodoListDto.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Todo.WebApi.Models.Dto;
+
+public record TodoListDto
+{
+	public TodoListDto(Guid id, string name, List<TodoItemDto>? items = null)
+	{
+		Id = id;
+		Name = name;
+		Tasks = items;
+	}
+
+	[JsonPropertyName("id")]
+	public Guid Id { get; init; }
+
+	[JsonPropertyName("name")]
+	public string Name { get; init; }
+
+	[JsonPropertyName("tasks")]
+	[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+	public List<TodoItemDto>? Tasks { get; set; } = null;
+}
