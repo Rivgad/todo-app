e15c984d26601634b491cfedce7808e02aa173ce
Rivgad <losvermo@gmail.com>
2023-10-03 19:49:29 +0300

    add TodoList and TodoItem

---
+++ src/Todo.Core/Models/TodoItem.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Todo.Core.Models;
+
+public class TodoItem
+{
+	public TodoItem(string name)
+	{
+		Name = name;
+	}
+
+	[Key]
+	public Guid Id { get; private set; } = Guid.NewGuid();
+
+	[Required]
+	[MaxLength(300)]
+	public string Name { get; set; }
+
+	[Required]
+	public TodoItemStatus Status { get; set; } = TodoItemStatus.Unfinished;
+
+	[Required]
+	[ForeignKey(nameof(TodoList))]
+	public Guid? TodoId { get; private set; }
+
+	public TodoList? Todo { get; private set; }
+}
