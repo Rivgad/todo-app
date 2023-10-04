fd195f34f45d52f43c5c146c69ca060fe4604162
Rivgad <losvermo@gmail.com>
2023-10-04 14:12:18 +0300

    add ClaimsPrincipalExtensions.GetUserId

---
+++ src/Todo.WebApi/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Claims;
+
+namespace Todo.WebApi.Extensions;
+
+public static class ClaimsPrincipalExtensions
+{
+	public static string GetUserId(this ClaimsPrincipal principal)
+	{
+		if (principal == null)
+			throw new ArgumentNullException(nameof(principal));
+
+		var claim = principal.FindFirst(ClaimTypes.NameIdentifier);
+
+		if (claim == null)
+			throw new KeyNotFoundException(nameof(ClaimTypes.NameIdentifier));
+
+		return claim.Value;
+	}
+}
