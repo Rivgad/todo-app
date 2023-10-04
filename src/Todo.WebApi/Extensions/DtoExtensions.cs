f8c8247824343836df5d58beb29489142335f822
Rivgad <losvermo@gmail.com>
2023-10-04 14:27:49 +0300

    move DtoExtensions to folder Extensions

---
+++ src/Todo.WebApi/Extensions/DtoExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Todo.Core.Models;
+using Todo.WebApi.Models.Dto;
+
+namespace Todo.WebApi.Extensions;
+
+public static class DtoExtensions
+{
+    public static TodoListDto ToDto(this TodoList item)
+        => new(item.Id, item.Name, item.Items.Select(item => item.ToDto()).ToList());
+
+    public static TodoItemDto ToDto(this TodoItem item)
+        => new(item.Id, item.Name, item.Status);
+}
